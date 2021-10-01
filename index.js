a5901ffaace91423a0ddc65cb3763308f09fe912
Nele Braun <nelebraun@Neles-MacBook-Air-2.local>
2021-10-01 11:18:38 +0200

    showTemprerature function, startSearch

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 let apiKey = "215576bab28022db35e6e64f040e1b56";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bremen&appid=${apiKey}&units=metric`;
+
+function searchCity(event) {
+  event.preventDefault();
+  console.log(inputSearchCity);
+}
+
+let startSearch = document.querySelector("#buttonGo");
+startSearch.addEventListener("submit", searchCity);
 
 function showTemperature(response) {
-  console.log(Math.round(response.data.main.temp));
-  let temperature = document.querySelector("#todayTemperature");
-  temperature.innerHtml = Math.round(response.data.main.temp);
+  document.querySelector("#todayTemperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#cityToday").innerHTML = response.data.name;
+  document.querySelector("#todayRain").innerHTML = response.data.main.humidity;
+  document.querySelector("#todayWind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 
 axios.get(apiUrl).then(showTemperature);
