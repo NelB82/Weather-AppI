1fe212fe6506c6fbb436a735a37d5765815546e6
Nele Braun <nelebraun@Neles-MacBook-Air-2.local>
2021-10-02 14:26:29 +0200

    added dayToday

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
+//function searchCity(event) {
+//event.preventDefault();
+//let city = document.querySelector("inputSearchcity");
+//document.querySelector("inputSearchCity.value").innerHTML = city.value;
+//let apiKey = "215576bab28022db35e6e64f040e1b56";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=metric`;
+//axios.get(apiUrl).then(showCity);
+//console.log(city);
+//}
+
+//let startSearch = document.querySelector("#searchForm");
+//startSearch.addEventListener("submit", searchCity);
+
+let city = document.querySelector("inputSearchCity");
 let apiKey = "215576bab28022db35e6e64f040e1b56";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bremen&appid=${apiKey}&units=metric`;
-
-function searchCity(event) {
-  event.preventDefault();
-  console.log(inputSearchCity);
-}
-
-let startSearch = document.querySelector("#buttonGo");
-startSearch.addEventListener("submit", searchCity);
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Jever&appid=${apiKey}&units=metric`;
 
 function showTemperature(response) {
   document.querySelector("#todayTemperature").innerHTML = Math.round(
     response.data.main.temp
   );
   document.querySelector("#cityToday").innerHTML = response.data.name;
-  document.querySelector("#todayRain").innerHTML = response.data.main.humidity;
+  document.querySelector("#todayHumidity").innerHTML =
+    response.data.main.humidity;
   document.querySelector("#todayWind").innerHTML = Math.round(
     response.data.wind.speed
   );
 }
 
 axios.get(apiUrl).then(showTemperature);
+
+//Datum
+
+let now = new Date();
+let today = document.querySelector("#dayToday");
+
+let weekDay = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let currentDay = weekDay[now.getDay()];
+today.innerHTML = currentDay;
