d5813d31c553ca0eeae1c7fa740e8f394daf3a6f
Nele Braun <nelebraun@Neles-MacBook-Air-2.local>
2021-10-02 14:37:29 +0200

    currentDate

--- index.js
+++ index.js
@@ -46,3 +46,26 @@ let weekDay = [
 
 let currentDay = weekDay[now.getDay()];
 today.innerHTML = currentDay;
+
+let dateNow = new Date();
+let dateToday = document.querySelector("#dateToday");
+let currentDate = dateNow.getDate();
+
+let Month = [
+  "01",
+  "01",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+];
+
+let currentMonth = Month[dateNow.getMonth()];
+let currentYear = dateNow.getFullYear();
+dateToday.innerHTML = `${currentDate}.${currentMonth}.${currentYear}`;
